e3aa6b27fbc54498003bccfce7cc0ad0096b2e06
Henrique Sato <henriquesato2003@gmail.com>
2023-10-28 17:09:37 -0300

    Criar endpoints

--- backend/potentialpancake/urls.py
+++ backend/potentialpancake/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import TodoListView, TaskView
+
+router = routers.DefaultRouter()
+router.register(r'todo-lists', TodoListView, 'todo-list')
+router.register(r'tasks', TaskView, 'task')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
