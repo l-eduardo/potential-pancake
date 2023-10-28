9e6bd51fb19ff70f8cbfbc9a092823700db0b206
Henrique Sato <henriquesato2003@gmail.com>
2023-10-28 16:46:27 -0300

    Criar serializadores

---
+++ backend/tasks/serializers/todo_list__serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from ..models import TodoList
+
+
+class TodoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoList
+        fields = ('id', 'title', 'description', 'created')
