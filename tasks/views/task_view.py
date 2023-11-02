6d12e2d50cd43d931b768d8bd678e8fe797f81cf
Luiz Eduardo da Silva <luiz.silva@srssistemas.com>
2023-11-02 17:35:27 -0300

    Deletando projeto do frontend, mudando tudo para django

--- tasks/views/task_view.py
+++ tasks/views/task_view.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from rest_framework import viewsets
+from django.template import loader
+
 from ..models import Task
 from ..serializers import TaskSerializer
 
@@ -6,3 +9,12 @@ from ..serializers import TaskSerializer
 class TaskView(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.all()
+
+
+    def get(self, request):
+        all_tasks = Task.objects.all()
+        template = loader.get_template("tasks/get_tasks.html")
+        context = {
+            "all_tasks": all_tasks,
+        }
+        return HttpResponse(template.render(context, request))
