8c937f0554dc7161d3a4115c8c4b090c523d4e45
Henrique Sato <henriquesato2003@gmail.com>
2023-11-19 15:56:07 -0300

    Permitir compartilhamento de cards

--- cards/forms.py
+++ cards/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Card
+from .models import Card, SharedCard
 
 
 class CardForm(forms.ModelForm):
     class Meta:
         model = Card
         fields = ('id', 'title', 'description', 'owner')
+
+
+class SharedCardForm(forms.ModelForm):
+    class Meta:
+        model = SharedCard
+        fields = ('card', 'shared_with', 'shared_group')
--- cards/models.py
+++ cards/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 
 class Card(models.Model):
@@ -9,3 +9,9 @@ class Card(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class SharedCard(models.Model):
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+    shared_with = models.ForeignKey(User, on_delete=models.CASCADE)
+    shared_group = models.ForeignKey(Group, on_delete=models.CASCADE)
