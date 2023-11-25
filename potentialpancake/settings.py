cb0cc28a698363d773cc1c0d1f78f9392ea62db0
Henrique Sato <henriquesato2003@gmail.com>
2023-11-25 11:46:28 -0300

    Resetar senha por email

--- potentialpancake/settings.py
+++ potentialpancake/settings.py
@@ -138,3 +138,22 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ORIGIN_WHITELIST = [
      'http://localhost:5173'
 ]
+
+# SMTP configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_USE_SSL = False
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+SERVER_EMAIL = os.environ.get('EMAIL_HOST_PASSWORD')
--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,4 @@
-from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django.contrib.auth.models import User
 from django import forms
 
@@ -8,3 +7,9 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class EmailOnlyPasswordResetForm(PasswordResetForm):
+    class Meta:
+        model = User
+        fields = ['email']
