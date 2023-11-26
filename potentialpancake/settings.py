29a8ed8c6f870d0899711c9d92276089dfa76b86
Luiz Eduardo da Silva <luiz.silva@srssistemas.com>
2023-11-26 13:02:45 -0300

    Troquei errado maluco

--- potentialpancake/settings.py
+++ potentialpancake/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_URL = '/login/'
+LOGIN_URL = 'user:login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
