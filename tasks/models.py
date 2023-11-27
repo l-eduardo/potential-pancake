280c2f9b55a1408f96f7c57c59f62a3b36305060
Henrique Sato <henriquesato2003@gmail.com>
2023-11-27 14:26:51 -0300

    remover atributos antigos do model

--- tasks/models.py
+++ tasks/models.py
@@ -4,7 +4,6 @@ from cards.models import Card
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=100)
     description = models.CharField(max_length=255)
     completed = models.BooleanField(default=False)
     card = models.ForeignKey(Card, on_delete=models.CASCADE)
